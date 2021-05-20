798987b9f062bafde29893c43e859784beb805db
Upadhyay <prayag.upd@gmail.com>
2021-05-19 22:46:27 -0700

    add code/decode example

--- src/main/java/ProtobufEncoders.java
+++ src/main/java/ProtobufEncoders.java
@@ -1,18 +1,35 @@
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ProtobufEncoders {
 
+    private static final String SERIALISED_FILE = "sku_data.bin";
+
     public static void main(String[] args) throws IOException {
-        var sku = SkuOuterClass.Sku.newBuilder()
-                .setId(1)
+
+        var stream = new FileOutputStream(SERIALISED_FILE);
+
+        var sku1 = SkuOuterClass.Sku.newBuilder()
+                .setId(10)
                 .setName("Macbook")
                 .setInventory(100)
                 .build();
 
-        var stream = new FileOutputStream("sku_data");
 
-        sku.writeTo(stream);
+        var sku2 = SkuOuterClass.Sku.newBuilder()
+                .setId(20)
+                .setName("iphone")
+                .setInventory(100)
+                .build();
+
+        sku1.writeTo(stream);
         stream.close();
+
+        //reader
+
+        var skuDecoded = SkuOuterClass.Sku.parseFrom(new FileInputStream(SERIALISED_FILE));
+        System.out.println("sku decoded ---------- \n" + skuDecoded);
+
     }
 }
